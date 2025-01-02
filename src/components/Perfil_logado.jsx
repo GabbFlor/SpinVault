091654ccd580de0977d2cbf6166d0307542047b8
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-01-02 14:18:43 -0300

    feat
    - Style página de perfil.

--- src/components/Perfil_logado.jsx
+++ src/components/Perfil_logado.jsx
@@ -5,9 +5,12 @@ import { doc, getDoc } from 'firebase/firestore';
 import { useQuery } from '@tanstack/react-query';
 import { tailChase } from 'ldrs';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Card_img from '../assets/card-img.png'
 
 const Perfil_logado = () => {
     const { user, loading } = useAuth();
+    const [ diferencaData, setDiferencaData ] = useState(0);
     tailChase.register()
 
     // função para pegar o perfil do usuario logado
@@ -34,15 +37,34 @@ const Perfil_logado = () => {
         staleTime: 1000 * 60 * 5,
     });
 
-    // converte o campo Criado_em (atualmente no tipo Timestamp) para String
-    const Criado_em_convertido = userProfile?.Criado_em?.toDate().toLocaleDateString('pt-BR');
+    // calcula a diferença entre a data de criação do perfil do usuário e a atual
+    useEffect(() => {
+        if (userProfile && userProfile.Criado_em) {
+            const calcularDiferenca = () => {
+                const now = Date.now();
+
+                // converte o Timestamp do firestore para MS para fazer a operação corretamente
+                const CriadoEm = userProfile.Criado_em.toMillis();
+    
+                const diferencaEmMs = now - CriadoEm;
+                const segundos = Math.floor(diferencaEmMs / 1000);
+                const minutos = Math.floor(segundos / 60);
+                const horas = Math.floor(minutos / 60);
+                const dias = Math.floor(horas / 24);
+    
+                setDiferencaData(dias)
+            };
+    
+            calcularDiferenca();
+        }
+    }, [userProfile]);
 
     if (isLoading) return (
         <div className="carregamento-perfil-page">
             <l-tail-chase
                 size="80"
                 speed="1.75" 
-                color="#C47D69"  
+                color="#fff"  
             ></l-tail-chase>
         </div>
     );
@@ -61,37 +83,66 @@ const Perfil_logado = () => {
     return (
         <section className='section-perfil'>
             <section className='perfil-infos'>
+                <div className="img-temporario">
+                    .
+                </div>
                 {/* IMAGEM DO PERFIL */}
 
                 <div>
-                    <h1>Nome do perfil</h1>
-                    <p>Localização do usuário</p>
+                    <h1>{userProfile.User_name}</h1>
+                    <p className='destaque'>Sp. Santo André</p>
                 </div>
 
                 <div className='sub-perfil-info'>
                     <div>
                         <p>Tempo de conta</p>
 
-                        <p><span>XX</span> dias</p>
+                        <p><span>{diferencaData}</span> {diferencaData > 1 ? ("dias") : ("dia")}</p>
                     </div>
 
                     <div>
                         <p>Adicionados</p>
 
                         {/* usar fonte maior do span de cima */}
-                        <p>XX</p>
+                        <span>12</span>
                     </div>
                 </div>
             </section>
 
             <section className='perfil-add-disks'>
+                <h1>Adicionados</h1>
 
+                <section className="cards-disks">
+                    <div className="card">
+                    <img src={Card_img} alt="Imagem do cartão" />
+
+                        <h1 className='card-title'>Vinil</h1>
+
+                        <button className='btn-card'>Editar</button>
+                    </div>
+
+                    <div className="card">
+                    <img src={Card_img} alt="Imagem do cartão" />
+
+                        <h1 className='card-title'>Vinil</h1>
+
+                        <button className='btn-card'>Editar</button>
+                    </div>
+
+                    <div className="card">
+                    <img src={Card_img} alt="Imagem do cartão" />
+
+                        <h1 className='card-title'>Vinil</h1>
+
+                        <button className='btn-card'>Editar</button>
+                    </div>
+                </section>
             </section>
         </section>
     )
 
     // Pegando os dados do perfil armazenados em cache:
-            // <p>Nome de usuário: {userProfile.User_name}</p>
+            // <p>Nome de usuário: </p>
             // <p>Tell: {userProfile.Tell}</p>
             // <p>Criado em: {Criado_em_convertido}</p>
     
